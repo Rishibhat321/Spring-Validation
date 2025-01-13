db7ded55f137908ea3e8202589ddb9c40b4dafe0
Rishib#321 <rishitab2209@gmail.com>
2025-01-13 13:25:08 +0530

    Spring mvc validation

---
+++ customer_Validation/src/main/java/com/spring/Validation/Customer.java
@@ -0,0 +1,71 @@
+package com.spring.Validation;
+
+import jakarta.validation.constraints.NotNull;
+
+public class Customer {
+
+    @NotNull(message="is required")
+    private String firstName;
+
+    @NotNull(message="is required")
+    private String lastName;
+
+    @NotNull(message="is required")
+    private String username;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String email;
+
+    @NotNull(message="is required")
+    private String address="";
+
+    private String country;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
---
+++ customer_Validation/src/main/java/com/spring/Validation/controller/CustomerController.java
@@ -0,0 +1,46 @@
+package com.spring.Validation.controller;
+
+import com.spring.Validation.Customer;
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class CustomerController {
+
+    // trims to Null in case of white-space strings
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+         dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+
+    @GetMapping("/showForm")
+    public String showForm(Model theModel) {
+
+        // create a customer instance
+        Customer theCustomer = new Customer();
+
+        // add to the model attribute
+        theModel.addAttribute("customer", theCustomer);
+
+        return "showForm";
+    }
+
+   @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult theBindingResult) {
+
+        if(theBindingResult.hasErrors()) {
+            return "showForm";
+        }
+
+        return "processForm";
+   }
+}
