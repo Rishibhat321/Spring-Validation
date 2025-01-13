4dedb9519e4755053ff10e1fd37b99e8123bc7c0
Rishib#321 <rishitab2209@gmail.com>
2025-01-13 13:25:36 +0530

    Spring mvc validation

--- Validation/src/main/java/com/spring/Validation/controller/CustomerController.java
+++ Validation/src/main/java/com/spring/Validation/controller/CustomerController.java
@@ -1,10 +1,14 @@
 package com.spring.Validation.controller;
 
 import com.spring.Validation.Customer;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomerController {
@@ -20,4 +24,15 @@ public class CustomerController {
 
         return "index";
     }
+
+   @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult theBindingResult) {
+
+        if(theBindingResult.hasErrors()) {
+            return "showForm";
+        }
+
+        return "processForm";
+   }
 }
