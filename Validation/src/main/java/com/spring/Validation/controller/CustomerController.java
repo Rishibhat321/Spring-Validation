262c062904679f686b9211fab8ccc98ba5ccd898
Rishib#321 <rishitab2209@gmail.com>
2025-01-13 13:56:29 +0530

    Spring mvc validation

--- Validation/src/main/java/com/spring/Validation/controller/CustomerController.java
+++ Validation/src/main/java/com/spring/Validation/controller/CustomerController.java
@@ -2,16 +2,23 @@ package com.spring.Validation.controller;
 
 import com.spring.Validation.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
 
     @GetMapping("/showForm")
     public String showForm(Model theModel) {
@@ -22,13 +29,15 @@ public class CustomerController {
         // add to the model attribute
         theModel.addAttribute("customer", theCustomer);
 
-        return "index";
+        return "showForm";
     }
 
    @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
                               BindingResult theBindingResult) {
 
+       System.out.println("Binding Results:" + " " + "|" +  theCustomer.getFirstName() + "|");
+       System.out.println("|" + theCustomer.getLastName() + "|");
         if(theBindingResult.hasErrors()) {
             return "showForm";
         }
